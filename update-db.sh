1a7ed3dae83b515eb2512efe81c1f2bf0ab22420
Ren W <ren+github@waldura.org>
2023-01-08 16:37:48 -0800

    update SQL database every hour

--- update-db.sh
+++ update-db.sh
@@ -2,6 +2,12 @@
 
 temp=/tmp/boinc-gui-$$.xml
 ~/boinc-gui/boinc_cluster_state.xml > $temp
+
+# update XML database
 basex -c "open boinc; add $temp"
+
+# update SQL database
+xsltproc sql.xsl $temp | sqlite3 ~/boinc-gui/boinc_cluster_state.db
+
 rm $temp
 
