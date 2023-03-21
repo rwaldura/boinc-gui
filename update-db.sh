12b4e742ccda5880436134b8636acd0d1ba55e83
Ren Waldura <ren+github@waldura.org>
2023-03-21 15:57:46 -0700

    capture messages and notices

--- update-db.sh
+++ update-db.sh
@@ -1,20 +1,28 @@
 #!/bin/sh
 
-temp=/tmp/boinc-gui-$$.xml
+temp=/tmp/boinc-gui-$$
+temp_state=$temp-state.xml
+temp_messages=$temp-messages.xml
+temp_notices=$temp-notices.xml
 
 cd ~/boinc-gui
 
-./boinc_cluster_state.xml > $temp && 
+./boinc_cluster_state.xml get_state > $temp_state && 
 	echo "dumped cluster state"
 
+./boinc_cluster_state.xml get_messages > $temp_messages && 
+	echo "dumped cluster messages"
+
+./boinc_cluster_state.xml get_notices > $temp_notices && 
+	echo "dumped cluster notices"
+
 # update SQL database
-xsltproc sql.xsl $temp | 
+xsltproc sql.xsl $temp_state $temp_messages $temp_notices | 
 	sqlite3 boinc_cluster_state.db &&
-		echo "updated SQL db"
+		echo "updated SQL database"
 
 # update XML database
-basex -c "open boinc; add $temp" &&
-	echo "updated XML db"
+basex -c "open boinc; add $temp_state" &&
+	echo "updated XML database"
 
-rm $temp
-
+echo rm $temp_state $temp_messages $temp_notices
